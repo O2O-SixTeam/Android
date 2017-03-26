85bb7a2f298a6869b796acca78a8c4ca4b2f365b
Jinsoo Park <wistnsos@gmail.com>
2017-03-26 15:47:19 +0900

    create LobbyActivity, SignActivity

---
+++ app/src/main/java/com/jspark/android/kardoc/SignActivity.java
@@ -0,0 +1,46 @@
+package com.jspark.android.kardoc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SignActivity extends AppCompatActivity {
+
+    TextView alertId, alertPw;
+    EditText editId, editPw;
+    Button btnSignin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+
+        setWidgets();
+
+        checkId();
+
+        setBtnSignin();
+    }
+
+    private void setWidgets() {
+        editId = (EditText)findViewById(R.id.editId);
+        editPw = (EditText)findViewById(R.id.editPw);
+        alertId = (TextView)findViewById(R.id.errorId);
+        alertPw = (TextView)findViewById(R.id.errorPw);
+        btnSignin = (Button)findViewById(R.id.btnSignin);
+    }
+
+    private void checkId() {
+        // make Logic to check Id&Pw
+    }
+
+    private void setBtnSignin() {
+        btnSignin.setOnClickListener((v) -> {
+            Intent i  = new Intent(SignActivity.this, LobbyActivity.class);
+            startActivity(i);
+        });
+    }
+}
